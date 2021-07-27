4e1212d4fd77b677131b4358ca20e9977f2b149a
SarahLongazel <sarah.longazel@gmail.com>
2021-07-26 22:06:03 -0400

    initial commit

---
+++ you_choose.py
@@ -0,0 +1,41 @@
+
+#story lines for stories
+house_list = [
+                "You are walking in the woods and see a haunted house. Do you...? \n 1. Go in. \n 2. Run to the beach."
+                "Entering the house you see... \n 1. Scarecrows \n 2. Monsters",
+                "You fight the scarecrows in the house and then the scarecrows get thirsty. \n They drink strawberry-pineanple-blueberry juice and it turns they into broccoli.\n The End.",
+                "You fight the monsters and they run away. You get hungry and make pizza. The pizza in magic and it makes you \n 1. Turn smaller \n 2. Ginormous.",
+                "You get smaller and people try to eat you because they think you're mice. \n  The End.",
+                "You turn ginormous and walk around the neighborhood and the neighbors get scared. \n  The End.",
+                "You run to the beach where there are sharks that want to eat you. Do you...? \n 1. Fight the sharks. \n 2. Run away on a motorbike.",
+                "You fight the sharks and they swim away. \n The End.",
+                "On the motorbike you go buy groceries. You buy grapes and eat them and you turn into grapes. Then people try to eat you \n and then they spit you out and say, 'Eew, human!' Then you turn back into a human. \n The End." 
+                ]
+unicorn_list = [
+                "You're walking in an enchanted forest and you see a unicorn surrounded by fire. To save the unicorn, do you...?\n 1. Jump over the fire. \n 2. Put the fire out.",
+                "You jump over the fire and ride the unicorn out of the fire. You get the unicorn water from \n 1. The Lake. \n 2. A bottle of water.",
+                "You and the unicorn fly to the lake and the unicorn gets water because it was hard for the unicorn to breath in the fire. \n The End.",
+                "You were holding a bottle of water and you give it to the unicorn. The bottle of water was magic water. \n 1. You drink it, too. \n 2. Just the unicorn drinks it.",
+                "You turn into a unicorn and the unicorn turns into you. In a weird whisper it says, 'We switched bodies.' \n The End.",
+                "The unicorn turns into Reese's Puffs and you eat it. \n The End.",
+                "You put the fire out and the unicorn eats a magic apple. Then the unicorn turns into \n 1. Sprite \n 2. A person.",
+                "The unicorn turns into Sprite and you drink it. You grow a horn and a tail. \n The End.",
+                "The unicorn turns into a person and you find two other unicorns and they get mad at the person unicorn. \n They start a war with lego people. \n The End."
+                ]
+
+while True:
+
+def which_story():
+
+#get the number choice from user and convert to choice varible
+def get_choice():
+    choice = int(input("Choose the number of what you want to do: "))
+    if choice == 1
+    return choice 
+
+#run the storyline choices based on returned choice variable
+def print_storyline():
+    print()
+
+def check_continue():
+
